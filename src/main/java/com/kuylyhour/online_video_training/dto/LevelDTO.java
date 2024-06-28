201b7895252fd2035157043b3f4be53bdcd50408
KuyLyhour24 <kuylyhour24programmer@gmail.com>
2024-06-28 14:25:06 +0700

    delete file

--- src/main/java/com/kuylyhour/online_video_training/dto/LevelDTO.java
+++
@@ -1,8 +0,0 @@
-package com.kuylyhour.online_video_training.dto;
-
-import lombok.Data;
-
-@Data
-public class LevelDTO {
- private String name;
-}
--- src/main/java/com/kuylyhour/online_video_training/entity/Video.java
+++ src/main/java/com/kuylyhour/online_video_training/entity/Video.java
@@ -10,9 +10,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name="videos")
+@Data
 public class Video {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
